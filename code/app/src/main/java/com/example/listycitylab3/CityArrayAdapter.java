ff17e4725a26c7e02c3745ee2f65f2884267922e
AngadSC <angads.chahil@gmail.com>
2025-09-18 13:30:57 -0600

    fixed imports

--- code/app/src/main/java/com/example/listycitylab3/CityArrayAdapter.java
+++ code/app/src/main/java/com/example/listycitylab3/CityArrayAdapter.java
@@ -1,7 +1,14 @@
 package com.example.listycitylab3;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import java.util.ArrayList;
 
 public class CityArrayAdapter extends ArrayAdapter<City> {
     public CityArrayAdapter(Context context, ArrayList<City> cities) {
